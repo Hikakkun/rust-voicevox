0de28529bd0874a970265ae9d4c37b28afc17776
Hikakkun <htrb0130@icloud.com>
2024-01-17 00:30:25 +0900

    mainを一部変更

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use vvcore::*;
 
 fn main() {
     let dir = std::ffi::CString::new("/usr/local/lib/voicevox_core/open_jtalk_dic_utf_8-1.11").unwrap();
-    println!("{:?}", dir);
     let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, true, dir.as_c_str()).unwrap();
 
     let text: &str = "こんにちはなのだ";
