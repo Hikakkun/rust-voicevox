65580b5933021f8eeb54a26cf45ee918e9868878
Hikakkun <htrb0130@icloud.com>
2024-01-17 00:17:18 +0900

    mainを追記

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::Write;
+use vvcore::*;
+
 fn main() {
-    println!("Hello, world!");
+    let dir = std::ffi::CString::new("/usr/local/lib/voicevox_core/open_jtalk_dic_utf_8-1.11").unwrap();
+    println!("{:?}", dir);
+    let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, true, dir.as_c_str()).unwrap();
+
+    let text: &str = "こんにちはなのだ";
+    let speaker: u32 = 1;
+    let wav = vvc.tts_simple(text, speaker).unwrap();
+
+    let mut file = std::fs::File::create("/home/hikaru/prog/hello-voicevox/audio.wav").unwrap();
+    file.write_all(&wav.as_slice()).unwrap();
 }
